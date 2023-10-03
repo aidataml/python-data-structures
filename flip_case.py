61b8ea7595fb19e3f641ab384f34d753b0babb84
aidataml <52681210+aidataml@users.noreply.github.com>
2023-10-03 08:18:13 -0400

    Added flip_case.py

---
+++ flip_case.py
@@ -0,0 +1,27 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+            
+    to_swap = to_swap.lower()
+    flipped_phrase = ""
+
+    for letter in phrase:
+        if letter.lower() == to_swap:
+            letter = letter.swapcase()
+        flipped_phrase += letter
+
+    return flipped_phrase
+
+print(flip_case('Aaaahhh', 'a'))
+print(flip_case('Aaaahhh', 'A'))
+print(flip_case('Aaaahhh', 'h'))
