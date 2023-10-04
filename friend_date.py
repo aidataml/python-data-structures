50dc4083be8bc962aadca98add73e8d946bbc4e5
aidataml <52681210+aidataml@users.noreply.github.com>
2023-10-04 12:26:24 -0400

    Added friend_date.py

---
+++ friend_date.py
@@ -0,0 +1,31 @@
+def friend_date(a, b):
+    """Given two friends, do they have any hobbies in common?
+
+    - a: friend #1, a tuple of (name, age, list-of-hobbies)
+    - b: same, for friend #2
+
+    Returns True if they have any hobbies in common, False if not.
+
+        >>> elmo = ('Elmo', 5, ['hugging', 'being nice'])
+        >>> sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+        >>> gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+
+        >>> friend_date(elmo, sauron)
+        False
+
+        >>> friend_date(sauron, gandalf)
+        True
+    """
+    # If the intersecton between the set 'a' list-of-hobbies and set 'b' list-of-hobbies is not empty, return true. 
+    # Else return false. 
+    
+    if set(a[2]) & set(b[2]):
+        return True
+    else:
+        return False
+
+elmo = ('Elmo', 5, ['hugging', 'being nice'])
+sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+print(friend_date(elmo, sauron))
+print(friend_date(sauron, gandalf))
